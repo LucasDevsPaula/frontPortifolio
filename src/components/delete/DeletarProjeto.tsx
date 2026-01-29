e3c7caace2a5cb2e162bc2182d677c30096ffe5b
TomoriComAncora <lucas.fernando@ufms.br>
2026-01-29 16:31:17 -0400

    Melhora o modal de exclusão de projeto com novos temas e estilos. Atualiza a exibição do nome do projeto e ajusta a estrutura do componente.

--- src/components/delete/DeletarProjeto.tsx
+++ src/components/delete/DeletarProjeto.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Modal, Button } from "flowbite-react";
 import { Trash2 } from "lucide-react";
 import toast from "react-hot-toast";
@@ -42,25 +42,47 @@ export const DeleteProjectModal = ({
     }
   };
 
-  return (
-    <Modal show={isOpen} size="md" onClose={onClose} popup>
-      <div className="p-6 text-center">
-        <Trash2 className="mx-auto mb-4 h-14 w-14 text-red-500 bg-red-50 rounded-full p-2" />
+  const customModalTheme = {
+    root: { base: "fixed inset-0 z-50 overflow-y-auto overflow-x-hidden" },
+    content: {
+      base: `bg-transparent`,
+    },
+  };
 
-        <h3 className="mb-2 text-lg font-bold text-gray-800">
+  const customButtonTheme = {
+    color: {
+      delete:
+        "text-white bg-detalhes hover:bg-[#805C48] transition-all cursor-pointer",
+      arquitetura:
+        "text-white bg-buttons hover:bg-buttonsHover transition-all cursor-pointer",
+    },
+  };
+
+  return (
+    <Modal
+      theme={customModalTheme}
+      show={isOpen}
+      size="md"
+      onClose={onClose}
+      popup
+    >
+      <div className="p-6 text-center rounded-2xl bg-secundary">
+        <Trash2 className="mx-auto mb-4 h-14 w-14 text-detalhes bg-inputs rounded-full p-2" />
+
+        <h3 className="mb-2 text-lg font-bold text-detalhes">
           Tem certeza absoluta?
         </h3>
-        <p className="mb-5 text-sm text-gray-500">
+        <p className="mb-5 text-sm text-detalhes">
           Você está prestes a excluir o projeto <b>{projectName}</b>. <br />
           Essa ação não pode ser desfeita.
         </p>
 
         <div className="flex justify-center gap-4">
-          <Button color="failure" onClick={handleDelete} disabled={isDeleting}>
+          <Button theme={customButtonTheme} color="delete" onClick={handleDelete} disabled={isDeleting}>
             {isDeleting ? "Excluindo..." : "Sim, excluir projeto"}
           </Button>
 
-          <Button color="gray" onClick={onClose} disabled={isDeleting}>
+          <Button theme={customButtonTheme} color="arquitetura" onClick={onClose} disabled={isDeleting}>
             Não, manter
           </Button>
         </div>
